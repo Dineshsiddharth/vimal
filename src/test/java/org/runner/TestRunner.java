89d233d8c447d26ea56b78cd42e439dcf92236a8
Dineshsiddharth <dinaexam7@gmail.com>
2022-05-21 13:15:36 +0530

    nithin

---
+++ src/test/java/org/runner/TestRunner.java
@@ -0,0 +1,12 @@
+package org.runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="C:\\Users\\ELCOT\\eclipse-workspace\\Maintenance\\src\\test\\resources\\FacebookLogin.Feature")
+public class TestRunner {
+
+}
